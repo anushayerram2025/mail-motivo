d917ddcd5c37c5916eac1916a487ef19f5b6afa4
anushayerram2025 <106475795+anushayerram2025@users.noreply.github.com>
2022-08-09 17:10:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require("express");
 const bodyParser=require("body-parser");
 //const request=require("request");
 const https=require("https");
-const app=express()
+const app=express();
 const my_email = "yerramanusha8@gmail.com"
 const    password = "sramyeyjffcvsrud"
 require("dotenv").config();
@@ -20,9 +20,9 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.post("/",function(req,res){
-  const email=req.body.email
-  const firstname=req.body.firstname
-  const lastname=req.body.lastname
+  const email=req.body.email;
+  const firstname=req.body.firstname;
+  const lastname=req.body.lastname;
 
   const data={
     members:[
@@ -32,19 +32,20 @@ app.post("/",function(req,res){
         merge_fields:{
           FNAME:firstname,
           LNAME:lastname
-        }
+        },
       }
     ]
   };
   //https.get(url,function()) ...this is useful to get data from the url not for hosting the data at the url location.
   const jasonData=JSON.stringify(data);
-  id_key=process.env.id
-  const aut="anusha:"+id_key
-  const url="https://us14.api.mailchimp.com/3.0/lists/fb0c4ae101"
+  id_key=process.env.id;
+  const aut="anusha:"+id_key;
+  const url="https://us14.api.mailchimp.com/3.0/lists/fb0c4ae101";
   const options={
     method:"POST",
     auth:aut
   }
+  console.log(aut);
 const request=https.request(url,options,function(response){
   console.log(response.statusCode);
 if (response.statusCode>=200 && response.statusCode<=299){
@@ -83,20 +84,7 @@ var transporter = nodemailer.createTransport({
   }
 });
 
-/*var mailOptions = {
-  from: 'youremail@gmail.com',
-  to: 'myfriend@yahoo.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-};
 
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});*/
 
 var nodemailer = require('nodemailer');
 
@@ -107,17 +95,7 @@ var transporter = nodemailer.createTransport({
     pass: password
   }
 });
-/*const fs = require('fs');
 
-const fileName = 'C:\Users\yerra\Newsletter-Signup\public\data.txt';
-fs.open(fileName);
-fs.readFile(fileName, 'utf8', (err, data) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log("anusha"+data);
-});*/
 
 var today = new Date().toLocaleTimeString("en-Us", {timeZone: 'Asia/Kolkata',hour12:false});
 if (today=="07:00:00" || today=="7:00:00"){
@@ -149,8 +127,4 @@ transporter.sendMail(mailOptions, function(error, info){
 })
 })
 }
-//api key
-//81fd75360841e98f6bc454df6b8ba65e-us14
 
-//list id
-//fb0c4ae101
