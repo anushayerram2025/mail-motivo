c5de96d414798610111c1d448cd2c10a6ad207c3
anushayerram2025 <106475795+anushayerram2025@users.noreply.github.com>
2023-10-12 16:18:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,87 @@
-const express=require("express");
-const bodyParser=require("body-parser");
-const https=require("https");
-const app=express();
-const my_email = "yerramanusha8@gmail.com"
-const password = "sramyeyjffcvsrud"
+const express = require("express");
+const mongoose = require("mongoose");
 const nodemailer = require("nodemailer");
+const cron = require("node-cron");
+const axios = require("axios");
+
 require("dotenv").config();
-app.listen(process.env.PORT||3000,function(){
-  console.log("The server has been started");
-})
-app.use(express.static("public"));
 
-app.get("/",function(req,res){
-  res.sendFile(__dirname+"/signup.html");
-})
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({extended:true}));
+// MongoDB Connection
+mongoose.connect("mongodb://localhost/your-database-name", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const User = mongoose.model("User", {
+  email: String,
+  firstname: String,
+  lastname: String,
+});
+
+// Nodemailer Configuration
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: Email,
+    pass: Password,
+  },
+});
+
+async function getMotivationalQuote() {
+  try {
+    const response = await axios.get("https://zenquotes.io/api/random");
+    if (response.data.length > 0) {
+      return response.data[0].q;
+    } else {
+      return "Stay motivated!";
+    }
+  } catch (error) {
+    console.error("Failed to fetch motivational quote:", error);
+    return "Stay motivated!";
+  }
+}
 
 
-
-app.post("/",function(req,res){
-  const email=req.body.email;
-  const firstname=req.body.firstname;
-  const lastname=req.body.lastname;
-
-  const data={
-    members:[
-      {
-        email_address:email,
-        status:"subscribed",
-        merge_fields:{
-          FNAME:firstname,
-          LNAME:lastname
-        },
-      }
-    ]
+function sendMotivationalEmail(user, quote) {
+  const mailOptions = {
+    from: "your-email@example.com",
+    to: user.email,
+    subject: "Your Daily Motivation",
+    text: `Hello ${user.firstname},\n\n${quote}\n\nStay motivated!`,
   };
 
-res.sendFile(__dirname+"/success.html")
-var mailOptions = {
-  from: my_email,
-  to: my_email,
-  subject:"New login in Motivo!!!!" ,
-  text: email
-};
-let transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: my_email,
-        pass: password
-      }
-    });
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(`Error sending email to ${user.email}:`, error);
+    } else {
+      console.log(`Motivational email sent to ${user.email}`);
+    }
+  });
+}
+
+cron.schedule("0 5 * * *", async () => {
+  console.log("Sending daily motivational emails...");
+
+  // Fetch a motivational quote
+  const quote = await getMotivationalQuote();
+
+  // Find all users in the database
+  User.find({}, (err, users) => {
+    if (err) {
+      console.error("Error fetching users from the database:", err);
+      return;
+    }
+
+    // Send motivational emails to all users
+    users.forEach((user) => sendMotivationalEmail(user, quote));
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 
-transporter.sendMail(mailOptions)
-
-
-
-
-})
